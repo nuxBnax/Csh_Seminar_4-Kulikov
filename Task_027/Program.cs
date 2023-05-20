9cd3b958c4a8ce4c4f3b399da291af338febde24
Kulikov Alexander <shouter84@yandex.ru>
2023-05-21 01:15:02 +0300

    Added solution fo Task_027

--- Task_027/Program.cs
+++ Task_027/Program.cs
@@ -5,3 +5,20 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
+Console.WriteLine("Enter ANY number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int sum = num;
+int n = 1;
+
+while ((num / n) > 0)
+{
+    sum = ((num % (n * 10)) - (num % n)) / n;
+    result += sum;
+    n *= 10;
+}
+
+Console.WriteLine(result);
--- Task_029/Program.cs
+++ Task_029/Program.cs
@@ -1,7 +1,62 @@
 ﻿// Задача 29:
 
 // Напишите программу, которая задаёт массив 
-// из элементов и выводит их на экран.
+// из нескольких элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// void Main()
+// {
+//     int size = ReadInt("Insert a size of array you need: ");
+//     int LeftRange = ReadInt("Insert LEFT range filling number of array: ");
+//     int RightRange = ReadInt("Insert RIGHT range filling number of array: ");
+//     FillArray(arr);
+// }
+
+// int ReadInt(string text)
+// {
+//     System.Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int[] FillArray(int[] size, int LeftRange, int RightRange)
+// {
+//     int[] arr = new int[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = rand.Next(LeftRange, RightRange);
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] Array)
+// {
+//     System.Console.WriteLine(string.Join(" ", Array));
+// }
+
+// int n = 8;
+// int[] arr = new int[n];
+// arr = FillArray(arr);
+// PrintArray(arr);
+
+
+void FillArray(int[] Array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = rand.Next(0, 10);
+    }
+}
+
+void PrintArray(int[] Array)
+{
+    System.Console.WriteLine(string.Join(", ", Array));
+}
+
+int n = 8;
+int[] arr = new int[n];
+FillArray(arr);
+PrintArray(arr);
 
