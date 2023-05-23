8a7e96f33e5bc5e70b7689d693d6729bf4def4ce
«Kulikov <shouter84@yandex.ru>
2023-05-23 22:09:21 +0300

    Fixed some code

--- Task_029/Program.cs
+++ Task_029/Program.cs
@@ -5,58 +5,48 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-// void Main()
-// {
-//     int size = ReadInt("Insert a size of array you need: ");
-//     int LeftRange = ReadInt("Insert LEFT range filling number of array: ");
-//     int RightRange = ReadInt("Insert RIGHT range filling number of array: ");
-//     FillArray(arr);
-// }
-
-// int ReadInt(string text)
-// {
-//     System.Console.Write(text);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
-
-// int[] FillArray(int[] size, int LeftRange, int RightRange)
-// {
-//     int[] arr = new int[size];
-//     Random rand = new Random();
-//     for (int i = 0; i < size; i++)
-//     {
-//         arr[i] = rand.Next(LeftRange, RightRange);
-//     }
-//     return arr;
-// }
-
-// void PrintArray(int[] Array)
-// {
-//     System.Console.WriteLine(string.Join(" ", Array));
-// }
-
-// int n = 8;
-// int[] arr = new int[n];
-// arr = FillArray(arr);
-// PrintArray(arr);
-
-
-void FillArray(int[] Array)
+void Main()
 {
+    int[] arr = FillArray(8, 1,10);
+    PrintArray(arr);
+}
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    int[] tempArray = new int[size];
     Random rand = new Random();
-    for (int i = 0; i < Array.Length; i++)
+
+    for (int i = 0; i < size; i++)
     {
-        Array[i] = rand.Next(0, 10);
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
     }
+    return tempArray;
 }
 
 void PrintArray(int[] Array)
 {
-    System.Console.WriteLine(string.Join(", ", Array));
+    System.Console.WriteLine("[" + string.Join(", ", Array) + "]");
 }
 
-int n = 8;
-int[] arr = new int[n];
-FillArray(arr);
-PrintArray(arr);
+Main();
+
+
+// void FillArray(int[] Array)
+// {
+//     Random rand = new Random();
+//     for (int i = 0; i < Array.Length; i++)
+//     {
+//         Array[i] = rand.Next(0, 10);
+//     }
+// }
+
+// void PrintArray(int[] Array)
+// {
+//     System.Console.WriteLine(string.Join(", ", Array));
+// }
+
+// int n = 8;
+// int[] arr = new int[n];
+// FillArray(arr);
+// PrintArray(arr);
 
